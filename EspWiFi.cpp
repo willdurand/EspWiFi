ab0731077f541b4723f507168431df34a3849e36
William Durand <will+git@drnd.me>
2015-03-25 02:05:57 +0100

    Import from coffuino project

---
+++ EspWiFi.cpp
@@ -0,0 +1,297 @@
+/* vim:ts=2 sw=2 sts=2 */
+/*
+ * EspEspWiFi - William Durand <will@drnd.me> - MIT License
+ */
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+
+#include "EspWiFi.h"
+
+EspWiFi::EspWiFi(HardwareSerial & serial, const unsigned int resetPin, const unsigned long baud) : serial_(& serial), resetPin_(resetPin), baud_(baud) {}
+
+#ifdef DEBUG
+EspWiFi::EspWiFi(HardwareSerial & serial, const unsigned int resetPin, SoftwareSerial & debug, const unsigned long baud) :  serial_(& serial), resetPin_(resetPin), debug_(& debug), baud_(baud) {}
+#endif
+
+void EspWiFi::begin()
+{
+  serial_->begin(baud_);
+
+  serial_->println(F("AT"));
+
+  if (! check_esp_response(1000)) {
+    debug("Error AT");
+    return;
+  }
+
+  hasBegun_ = true;
+}
+
+bool EspWiFi::connect(String ssid, String password)
+{
+  if (! hasBegun_) {
+    begin();
+  }
+
+  // 1 = client
+  serial_->println(F("AT+CWMODE=1"));
+
+  if (! check_esp_response(1000)) {
+    debug("Error AT+CWMODE");
+    return false;
+  }
+
+  if (! reset()) {
+    debug("Error reset() in connect()");
+    return false;
+  }
+
+  serial_->println(F("AT+CWJAP?"));
+
+  if (! check_esp_response(1000, ssid)) {
+    serial_->print(F("AT+CWJAP=\""));
+    serial_->print(ssid);
+    serial_->print(F("\",\""));
+    serial_->print(password);
+    serial_->println(F("\""));
+
+    if (! check_esp_response(5000)) {
+      debug("Error AT+CWJAP");
+      return false;
+    }
+  }
+
+  return true;
+}
+
+bool EspWiFi::startAccessPoint(String ssid, String password)
+{
+  if (! hasBegun_) {
+    begin();
+  }
+
+  // 3 = client + AP
+  serial_->println(F("AT+CWMODE=3"));
+
+  if (! check_esp_response(1000)) {
+    debug("Error AT+CWMODE");
+    return false;
+  }
+
+  if (! reset()) {
+    debug("Error reset() in startAccessPoint()");
+    return false;
+  }
+
+  serial_->print(F("AT+CWSAP=\""));
+  serial_->print(ssid);
+  serial_->print(F("\",\""));
+  serial_->print(password);
+  serial_->println(F("\",3,0"));
+
+  if (! check_esp_response(1000)) {
+    debug("Error AT+CWSAP");
+    return false;
+  }
+
+  return true;
+}
+
+bool EspWiFi::listen(int port)
+{
+  serial_->println(F("AT+CIPMUX=1"));
+
+  if (! check_esp_response(1000)) {
+    debug("Error AT+CIPMUX");
+    return false;
+  }
+
+  serial_->print(F("AT+CIPSERVER=1,"));
+  serial_->println(port);
+
+  if (! check_esp_response(1000, F("OK"), F("no change"))) {
+    debug("Error AT+CIPSERVER");
+    return false;
+  }
+
+  return true;
+}
+
+String EspWiFi::read(unsigned int mux_id, int length, unsigned long timeout)
+{
+  String data;
+
+  bool isBlank    = false;
+  unsigned long t = millis();
+
+  char c;
+  while (millis() - t < timeout) {
+    if (serial_->available()) {
+      c = serial_->read();
+
+      if (c == '\n' && isBlank) {
+        break;
+      }
+
+      if (c == '\n') {
+        isBlank = true;
+      }
+      else if (c != '\r') {
+        isBlank = false;
+      }
+
+      data += c;
+    }
+  }
+
+  if (data.startsWith("+IPD,")) {
+    // request: +IPD,ch,len:data
+    sscanf(data.substring(5, data.indexOf(":")).c_str(), "%d,%d", &mux_id, &length);
+    data = data.substring(data.indexOf(":") + 1);
+
+    t = millis();
+    while (data.length() < length && millis() - t < timeout) {
+      if (serial_->available()) {
+        c = serial_->read();
+        data += c;
+      }
+    }
+
+    return data;
+  }
+
+  return "";
+}
+
+bool EspWiFi::write(const unsigned int mux_id, String data)
+{
+  serial_->print(F("AT+CIPSEND="));
+  serial_->print(mux_id);
+  serial_->print(",");
+  serial_->println(data.length());
+
+  if (check_esp_response(2000, F(">"))) {
+    serial_->print(data);
+
+    return check_esp_response(1000, F("SEND OK"));
+  }
+
+  return close(mux_id);
+}
+
+bool EspWiFi::close(const unsigned int mux_id)
+{
+  serial_->print(F("AT+CIPCLOSE="));
+  serial_->println(mux_id);
+
+  return check_esp_response(1000);
+}
+
+void EspWiFi::end()
+{
+  serial_->end();
+  hasBegun_ = false;
+}
+
+bool EspWiFi::reset()
+{
+  serial_->println(F("AT+RST"));
+
+  if (! check_esp_response(1000, F("OK"))) {
+    debug("Error AT+RST");
+    return false;
+  }
+
+  unsigned long t = millis();
+  while (millis() - t < 10000) {
+    serial_->println(F("AT"));
+
+    if (check_esp_response(1000)) {
+      delay(1000);
+      return true;
+    }
+
+    delay(500);
+  }
+
+  return false;
+}
+
+void EspWiFi::hardReset()
+{
+  end();
+
+  digitalWrite(resetPin_, LOW);
+  delay(1000);
+  digitalWrite(resetPin_, HIGH);
+  delay(3000);
+}
+
+bool EspWiFi::check_esp_response(unsigned long timeout, String response)
+{
+  char c;
+  String buffer;
+  bool found = false;
+  unsigned long t = millis();
+
+  while (millis() - t < timeout) {
+    if (serial_->available() > 0) {
+      c = serial_->read();
+      buffer += c;
+
+      if (buffer.indexOf(response) != -1 || buffer.equals(response)) {
+        found = true;
+
+        break;
+      }
+    }
+  }
+
+  while(serial_->available() > 0) {
+    c = serial_->read();
+    buffer += c;
+  }
+
+  debug(buffer);
+
+  return found;
+}
+
+bool EspWiFi::check_esp_response(unsigned long timeout, String response, String altResponse)
+{
+  char c;
+  String buffer;
+  bool found = false;
+  unsigned long t = millis();
+
+  while (millis() - t < timeout) {
+    if (serial_->available() > 0) {
+      c = serial_->read();
+      buffer += c;
+
+      if (buffer.indexOf(response) != -1 || buffer.equals(response)
+        || buffer.indexOf(altResponse) != -1 || buffer.equals(altResponse)) {
+        found = true;
+
+        break;
+      }
+    }
+  }
+
+  while(serial_->available() > 0) {
+    c = serial_->read();
+    buffer += c;
+  }
+
+  debug(buffer);
+
+  return found;
+}
+
+void EspWiFi::debug(String content)
+{
+#ifdef DEBUG
+  debug_->println(content);
+  delay(100);
+#endif
+}
---
+++ EspWiFi.h
@@ -0,0 +1,49 @@
+/* vim:ts=2 sw=2 sts=2 */
+/*
+ * EspWiFi - William Durand <will@drnd.me> - MIT License
+ *
+ * WIFI driver for ESP8266 ESP-01 (well, at least tested with it)
+ */
+#ifndef ESP_WIFI_H
+#define ESP_WIFI_H
+
+class String;
+class HardwareSerial;
+class SoftwareSerial;
+
+class EspWiFi
+{
+  public:
+    EspWiFi(HardwareSerial & serial, const unsigned int resetPin, const unsigned long baud = 115200);
+#ifdef DEBUG
+    EspWiFi(HardwareSerial & serial, const unsigned int resetPin, SoftwareSerial & debug, const unsigned long baud = 115200);
+#endif
+
+    void begin();
+    bool connect(String ssid, String password);
+    bool startAccessPoint(String ssid, String password);
+    bool listen(int port);
+    bool reset();
+    void end();
+    void hardReset();
+
+    String read(unsigned int mux_id, int length, unsigned long timeout = 2000);
+    bool write(const unsigned int mux_id, String data);
+    bool close(const unsigned int mux_id);
+
+  private:
+    bool hasBegun_ = false;
+    const unsigned long baud_;
+    const unsigned int resetPin_;
+    HardwareSerial *serial_;
+#ifdef DEBUG
+    SoftwareSerial *debug_;
+#endif
+
+    bool check_esp_response(unsigned long timeout, String response = "OK");
+    bool check_esp_response(unsigned long timeout, String response, String altResponse);
+
+    void debug(String content);
+};
+
+#endif
