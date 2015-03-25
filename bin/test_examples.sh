8594233a7928ea18c23f6bfb723bdc280ec3b962
William Durand <will+git@drnd.me>
2015-03-26 00:33:16 +0100

    Fix test_examples.sh script

--- bin/test_examples.sh
+++ bin/test_examples.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
+CURRENT_DIR=`pwd`
+
 for example in $(ls examples/)
 do
     echo "---------------------------------------------------------------------------------"
     echo " Building: $example"
     echo "---------------------------------------------------------------------------------"
 
-    cp Makefile _Makefile.master "examples/$example/"
-    cd !$
+    example_dir="examples/$example/"
+
+    cp Makefile _Makefile.master "$example_dir"
+    cd "$example_dir"
     make
-    cd -
+    cd "$CURRENT_DIR"
 done
