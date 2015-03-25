1a5987d36c95d7248c1076930f4209b5e6315e50
William Durand <will+git@drnd.me>
2015-03-26 00:11:59 +0100

    Add example

---
+++ examples/client/client.ino
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+#include <EspWiFi.h>
+
+// Wired to `RESET` pin on the ESP8266
+#define RESET_PIN 6
+
+#define SSID     "Your-SSID"
+#define PASSWORD "P4ssw0rd"
+#define TCP_PORT 80
+
+EspWiFi wifi(Serial, RESET_PIN);
+
+void setup() {
+  pinMode(RESET_PIN, OUTPUT);
+  
+  if (wifi.connect(SSID, PASSWORD)) {
+    if (! wifi.listen(TCP_PORT)) {
+      // TODO: error
+    }
+  } else {
+    // TODO: error
+  }
+}
+
+void loop() {
+  unsigned int mux_id, len;
+  String data = wifi.read(mux_id, len);
+  
+  // send received data back
+  if (data.length() > 0) {
+    wifi.write(mux_id, data);
+  }
+}
